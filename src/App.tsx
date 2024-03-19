2994970365098c54450c1e5aae4104a9507b4ef0
anirudh <anirudh04mishra@gmail.com>
2024-03-19 11:34:27 +0530

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+      <p>Hello</p>
+    </>
+  );
+}
+
+export default App;
